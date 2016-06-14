61ff232f290750007ddedc2ad0eaed4edf4511a6
CK <thecurrencykid@gmail.com>
2016-06-13 22:42:23 -0700

    5000 fake records with faker & lodash packages

--- server/main.js
+++ server/main.js
@@ -1,5 +1,33 @@
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
+import {  Employees } from '../imports/collections/employees'; 
+import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+
+  //we can generate data in here
+ 
+  // check to see if data exists in the collection 
+  // see if collection has any records
+
+  const numberRecords = Employees.find({}).count(); 
+
+  console.log(numberRecords); 
+
+  if (!numberRecords){
+    //generate data
+
+    _.times(5000, () => {
+        const {name, email, phone} = helpers.createCard(); 
+
+        Employees.insert({
+          // if value and key are identical u only need to put key
+          name,
+          email,
+          phone,
+          avatar: image.avatar()
+        });
+    }); 
+  }
+
 });
